837032769e0a14ff3e6d289a33fc301443ce38e1
MahdiHasan943 <mahdifreelancer41@gmail.com>
2023-02-14 21:29:43 +0600

    fix hero section design problem

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -19,21 +19,21 @@ https://edmy-react.hibootstrap.com/images/banner/shape-1.svg */}
 
           <div className="px-8  2xl:px-[100px]  bg-[#39a1ae]  lg:bg-gradient-to-r from-[#96d1a1] to-[#36a0ad] " >
         <div className="  relative 	">
-          <div className="md:flex  py-[80px]">
+          <div className="lg:flex  pt-[80px] herop pb-[150px]">
           {/* py-[30px] */}
                   
-                  <div className="relative pt-8 md:w-[50%] ">
-                      <div className="  z-0 bg-gradient-to-l from-[#7cc3ad] to-[#b4dec2] overflow-hidden  h-[350px] sm:h-[500px] w-[100%] md:h-[500px] lg:h-[600px] lg:w-[100%] opacity-bottom-40 xl:w-[750px] xl:h-[700px] rounded-[100%]">
+                  <div className="relative mx-auto pt-8 lg:w-[50%] ">
+                      <div className="z-0  lg:m bg-gradient-to-l from-[#7cc3ad] bg to-[#b4dec2] overflow-hidden  opacity-bottom-40 rounded-[100%]">
                           {/* className='h-[330px] w-[100%] girl animate-pop-in  sm:h-[400px] md:h-[400px] lg:h-[400px] xl:h-[600px]'         */}
               </div>
-              <img className='absolute w-[] z-40 left-[30px] bottom-[50px] '  src="https://edmy-react.hibootstrap.com/images/banner/banner-img-1.png" alt="" />
+              <img className='absolute lg:mx-none  heroimg z-40 left-[30px] bottom-[50px] '  src="https://edmy-react.hibootstrap.com/images/banner/banner-img-1.png" alt="" />
 
                   </div>
-                  <div className="md:w-[50%] px-8 py-12 ">
+                  <div className="lg:w-[50%] text-center  lg:text-start px-8 py-12 ">
                       
                 <motion.img 
-                  className=' kham ml-[-30px] mt-[-30px]' src="https://edmy-react.hibootstrap.com/images/banner/shape-1.svg " alt="" />
-                      <h1 className='text-white animate-pop-in title font-bold text-[30px]  lg:text-[46px] xl:text-[60px]'>Improve Your Online <br /> Learning Experience <br /> Better Instantly </h1>
+                  className=' kham ml-[-40px] mt-[-50px]' src="https://edmy-react.hibootstrap.com/images/banner/shape-1.svg " alt="" />
+                      <h1 className='text-white animate-pop-in lg:tracking-[-4px] py-3 title font-bold text-[30px]  lg:text-[46px]  2xl:text-[70px]'>Improve Your Online <br className='hidden lg:block' /> Learning Experience <br className='hidden lg:block'  /> Better Instantly </h1>
                       {/* <h1 className='text-white font-bold  text-[60px]'>Improve Your Online   </h1>
                       <h1 className='text-white font-bold  text-[60px]'> Learning Experience   </h1>
                       <h1 className='text-white font-bold  text-[60px]'> Better Instantly    </h1> */}
@@ -42,9 +42,9 @@ https://edmy-react.hibootstrap.com/images/banner/shape-1.svg */}
                           <p className='text-white subtitle animate-pop-in font-semibold  text-[18px]'>We have <span className='text-[#fec400]'>40k+</span> Online courses & <span className='text-[#fec400]'> 500K++</span>  Online registered student. Find your <br /> desired Courses from them.</p>
                           
     
-                          <div className="form-control py-8 w-full">
-      <div className="input-group animate-pop-in group pr-[100px]">
-        <input type="text" placeholder="Search Courses" className="input focus:outline-none placeholder-[#d6f0f2] py-7  text-white bg-[#7dc2c4] input-bordered" />
+                          <div className="form-control  py-8 w-full">
+      <div className="input-group flex justify-center animate-pop-in group lg:pr-[100px]">
+        <input type="text" placeholder="Search Courses" className="input sm:px-8 focus:outline-none placeholder-[#d6f0f2] py-7  text-white bg-[#7dc2c4] input-bordered" />
         
                                   <button className="flex  h-[57px]  border-0 hover:outline-none hover:bg-[#f0bf79] capitalize text-black hover:border-none outline-none  btn bg-[#f0bf79] ">
                                       Search Now
@@ -52,34 +52,34 @@ https://edmy-react.hibootstrap.com/images/banner/shape-1.svg */}
         </button>
       </div>
     </div>
-                          <div className=" animate-pop-in little items-center sm:flex">
-                          <div className="avatar-group justify-center  pr-4 -space-x-6">
+                          <div className=" animate-pop-in lg:flex justify-center lg:justify-start little items-center sm:flex">
+                          <div className="avatar-group justify-center  pr-5 -space-x-5">
       <div className="avatar ">
-        <div className="w-12">
+        <div className="w-11  	">
           <img className='' src="https://edmy-react.hibootstrap.com/images/banner/client-3.jpg" />
         </div>
       </div>
       <div className="avatar ">
-        <div className="w-12">
+        <div className="w-11">
           <img src="https://edmy-react.hibootstrap.com/images/banner/client-1.jpg" />
         </div>
       </div>
       <div className="avatar">
-        <div className="w-12">
+        <div className="w-11">
           <img src="https://edmy-react.hibootstrap.com/images/banner/client-2.jpg" />
         </div>
       </div>
       
     </div>
                               
-                          <p className='text-white py-4 sm:py-0 flex items-center font-semibold  text-[18px]'>500K+ People already trusted us.<span><Link to={'/'} className="underline p-1 text-[#fec400]  " >View Courses  </Link></span><BsArrowRightShort className='font-extrabold text-3xl mx-2 text-[#fec400]'></BsArrowRightShort> </p>
+                          <p className='text-white py-4 sm:py-0 sm:flex items-center font-semibold  text-[18px]'>500K+ People already trusted us.<p className='flex justify-center'><Link to={'/'} className="underline p-1 text-[#fec400]  " >View Courses  </Link> <BsArrowRightShort className='font-extrabold text-3xl mx-2 text-[#fec400]'></BsArrowRightShort></p> </p>
     
                 </div>
               
               </div>
     
           </div>
-          <div className="hidden absolute bottom-5 right-0  sm:flex sm:justify-end ">
+          <div className="hidden absolute bottom-20 right-0  sm:flex sm:justify-end ">
                   <img className='book' src="https://edmy-react.hibootstrap.com/images/banner/shape-3.svg" alt="" />
           </div>
           <img className='hidden  trophy lg:block absolute mt-[-50px] bottom-90 left-20 ' src="https://edmy-react.hibootstrap.com/images/banner/shape-2.svg" alt="" />
